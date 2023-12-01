d6756898370400dca6a684847a9e6e0d021dbe60
PRishabhMishra <prishabh1999@gmail.com>
2023-12-01 08:36:56 -0800

    Code Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,12 @@ package com.driver;
 public  class Main {
     public static void main(String[] args) {
         A objA = new A();
-        System.out.println(objA.meth());
+        objA.meth();
 
         B objB = new B();
-        System.out.println(objB.callMethodFromA());
+        objB.callMethodFromA();
 
         B objB1 = new B();
-        System.out.println(objB1.meth());
+        objB1.meth();
     }
 }
